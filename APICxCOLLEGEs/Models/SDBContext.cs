162cbff1be30f4d534064a53b006ca85a8a261b6
Sahira22 <60494004+Sahira22@users.noreply.github.com>
2020-03-27 23:35:55 -0400

    first commit

---
+++ APICxCOLLEGEs/Models/SDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace APICxCOLLEGEs.Models
+{
+    public class SDBContext: DbContext
+    {
+        public SDBContext(DbContextOptions<SDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
---
+++ APICxCOLLEGEs/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICxCOLLEGEs.Models
+{
+    public class Student
+    {
+
+
+        public long Id { get; set; }
+        public string Nombre { get; set; }
+        public string Asignatura { get; set; }
+
+        public string Carrera { get; set; }
+
+
+    }
+}
